0be3864784e7a1f6a9c8437d607ff44b383a1589
Kundan <kundan.kk0202@gmail.com>
2020-11-19 19:22:49 +0530

    CSL-5:Added Encode functinality

---
+++ Encode.java
@@ -0,0 +1,17 @@
+class Encode{
+ // create a sample String to encode 
+        String sample = "India Team will win the Cup"; 
+  
+        // print actual String 
+        System.out.println("Sample String:\n"
+                           + sample); 
+  
+        // Encode into Base64 format 
+        String BasicBase64format 
+            = Base64.getEncoder() 
+                  .encodeToString(sample.getBytes()); 
+  
+        // print encoded String 
+        System.out.println("Encoded String:\n"
+                           + BasicBase64format); 
+}
